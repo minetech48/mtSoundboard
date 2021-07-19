07f4b9561c4bc666dd6208b48fb7656846bab388
minetech48 <minetech48@gmail.com>
2021-07-18 19:20:05 -0500

    Delete Soundboard V4 directory

--- Soundboard V4/resources/gui/scripts/GUIMain.lua
+++
@@ -1,5 +0,0 @@
-EventBus = luajava.bindClass("engine.core.EventBus")
---GUI = luajava.bindClass("engine.ux.GUI")
-
-EventBus:broadcast({"GUILoadData"})
-EventBus:broadcast({"GUIShow", "SoundBoard"})
